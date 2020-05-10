683f45e6123ad41ad404845a4556a861278270c2
smile2.sm@qq.com <smile2.sm@qq.com>
2020-05-10 22:29:51 +0800

    修改

--- SpringCloudConfig/SpringConfigTest/src/main/java/com/smile2sm/controller/indexController.java
+++ SpringCloudConfig/SpringConfigTest/src/main/java/com/smile2sm/controller/indexController.java
@@ -2,10 +2,12 @@ package com.smile2sm.controller;
 
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class indexController {
 
     @Value("${hello}")
