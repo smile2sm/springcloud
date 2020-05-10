95dfff87d3066313a3e4eb4188fb86e1bc925dc7
smile2.sm@qq.com <smile2.sm@qq.com>
2020-05-10 19:23:53 +0800

    init

---
+++ SpringCloudConfig/SpringConfigTest/src/main/java/com/smile2sm/controller/indexController.java
@@ -0,0 +1,19 @@
+package com.smile2sm.controller;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class indexController {
+
+    @Value("${hello}")
+    String helloStr;
+
+    @RequestMapping("/")
+    public String index(){
+        System.out.println("读取到的配置为:" + helloStr);
+        return "读取到的配置为:" + helloStr;
+    }
+}
